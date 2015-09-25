12b3b1daa0772858be82a4f05e5dbe562194ab6c
unknown <tjl7@KAY.csad.cs.calvin.edu>
2015-09-25 14:39:38 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tjl7/lab1/MyActivity.java
@@ -0,0 +1,50 @@
+package com.example.tjl7.lab1;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_my, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openSearch(){
+        startActivity(new Intent(SearchManager.INTENT_ACTION_GLOBAL_SEARCH));
+    }
+
+    private void openSettings(){
+        startActivity(new Intent(Settings.ACTION_SETTINGS));
+    }
+}
