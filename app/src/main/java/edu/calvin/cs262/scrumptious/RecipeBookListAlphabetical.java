94ccc35033395f2d997f2db1bed99f5f535f3f04
kirbyfreak29 <kirby_freak@mac.com>
2015-11-22 23:42:18 -0500

    Got rid of the settings image
    I commented out the settings bar in the top right.  I think this is
    where we’d be able to put an add image in.  I’ll figure out how to do
    that tomorrow.

--- app/src/main/java/edu/calvin/cs262/scrumptious/RecipeBookListAlphabetical.java
+++ app/src/main/java/edu/calvin/cs262/scrumptious/RecipeBookListAlphabetical.java
@@ -39,26 +39,26 @@ public class RecipeBookListAlphabetical extends Activity {
         listView.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_recipe_book_list_alphabetical, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_recipe_book_list_alphabetical, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
